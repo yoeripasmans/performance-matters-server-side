632ef8d3af2c1a7bcc79e9bd73bc9c07ff17525d
Yoeri Pasmans <yoerip@live.nl>
2018-03-20 12:18:32 +0100

    Setup

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var MapboxClient = require('mapbox');
+var client = new MapboxClient('pk.eyJ1IjoieW9lcndlbGkiLCJhIjoiY2plZmViOHNlMWhhazMza3R5MmE5NG9jcyJ9.Bm8bYiLXnQ41Nv_Sio1xpA');
 var express = require('express');
 var request = require('request');
 var app = express();
@@ -6,18 +8,11 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/', function (req, res) {
-    res.render('index.ejs');
+app.get('/', function(req, res) {
+	res.render('index.ejs');
 
 });
 
-app.get('/movies/:id', function (req, res) {
-  request(host + req.params.id, function (error, response, body) {
-    var data = JSON.parse(body);
-    res.render('detail.ejs', {movie: data});
-  });
-});
-
-var server = app.listen(3000, function () {
-   console.log('server is running on port 3000');
+app.listen(3000, function() {
+	console.log('server is running on port 3000');
 });
